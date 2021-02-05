5a17150629ff9746353ce798f8c157f82a486282
Kebron <ztkmkoo@gmail.com>
2021-02-05 15:31:28 +0900

    no message

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	route := gin.Default()
+
+	route.Use(static.Serve("/", static.LocalFile("./frontend/build", true)))
+
+	api := route.Group("/api")
+	{
+		api.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "pong",
+			})
+		})
+	}
+
+	route.Run(":5000")
+}
